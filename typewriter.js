4c2423e1cb1d1f5b532900cece124e72f0755961
murray-pocha <murray.pocha@gmail.com>
2024-10-30 16:05:25 -0600

    built function and fixed bugs

---
+++ typewriter.js
@@ -0,0 +1,17 @@
+const sentence = 'Hello there from lighthouse labs!';
+
+let delay = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+
+  delay += 50;
+
+}
+
+setTimeout(() => {
+  console.log();
+}, delay);
+
